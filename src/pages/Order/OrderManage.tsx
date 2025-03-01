c2329a6959c9bd8005dd4ef6fa5acfd05466d74e
behzad 1k <bhzd1k@gmail.com>
2025-03-01 19:02:41 +0330

    fixes

--- src/pages/Order/OrderManage.tsx
+++ src/pages/Order/OrderManage.tsx
@@ -340,7 +340,7 @@ const OrderManage = () => {
               label: orderStatus[Object.keys(orderStatus).find(e => e == form?.status)]
             }} options={Object.entries(orderStatus).map(([key, value]) => ({value: key, label: value}))} className="/dashCardLog" id="infoTitle" onChange={(selected) => setForm(prev => ({ ...prev, status: selected.value }))}/>
             <label className="sideBarTitle">تاریخ</label>
-            <DatePicker inputClass="editProductInput" locale={persian_fa} value={form?.date} onChange={(e) => setForm(prev => ({ ...prev, date: tools.persianNumToEn(e.format('YYYY/MM/DD')) }))} />
+            <DatePicker inputClass="editProductInput"  weekStartDayIndex={7} locale={persian_fa} value={form?.date} onChange={(e) => setForm(prev => ({ ...prev, date: tools.persianNumToEn(e.format('YYYY/MM/DD')) }))} />
             <label className="sideBarTitle">ساعت</label>
             <input className="editProductInput" value={form?.time} onChange={(input) => setForm(prev => ({ ...prev, time: input.target.value}))}/>
             <div className='inputRow'>
